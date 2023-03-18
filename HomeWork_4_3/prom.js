e80dcb59401722db9a67afa5960ca977654e50a0
MSergey86 <105745751+MSergey86@users.noreply.github.com>
2023-03-18 12:07:45 +0300

    second commit
    added try/catch

--- HomeWork_4_3/prom.js
+++ HomeWork_4_3/prom.js
@@ -16,30 +16,37 @@ async function input() {
         console.log('Попытка №: ', count);
         const number = await question('Введите цифру от 0 до 1000 ');
         console.log('Вы ввели ', number);
-        if (number == 'quit') {
-            rl.close();
-            console.log('Игра завершена, количесвто попыток: ', count-1);
-            break;
+        try {
+            if (number == 'quit') {
+                rl.close();
+                console.log('Игра завершена, количесвто попыток: ', count-1);
+                break;
+            }
+            else if (isNaN(number)) {
+                console.log("Вы должны ввести чило")
+            }
+            else if (+number > 1000 || number < 0 ){
+                console.log("Вы должны ввести число от 0 до 1000")
+            }
+            else if (+number < random) {
+                console.log("Заданное число больше введенного")
+            }
+            else if (+number > random) {
+                console.log("Заданное чило меньше введенного")
+            }
+            else {
+                console.log("Вы угадали!!!");
+                console.log('Количество попыток: ', count);
+                rl.close();
+                break;
+            }
+            count++;
         }
-        else if (isNaN(number)) {
-            console.log("Вы должны ввести чило")
+        catch (err) {
+            console.log(err);
+        } finally {
+            console.log("------");
         }
-        else if (+number > 1000 || number < 0 ){
-            console.log("Вы должны ввести число от 0 до 1000")
-        }
-        else if (+number < random) {
-            console.log("Заданное число больше введенного")
-        }
-        else if (+number > random) {
-            console.log("Заданное чило меньше введенного")
-        }
-        else {
-            console.log("Вы угадали!!!");
-            console.log('Количество попыток: ', count);
-            rl.close();
-            break;
-        }
-        count++;
     }
 }
 
