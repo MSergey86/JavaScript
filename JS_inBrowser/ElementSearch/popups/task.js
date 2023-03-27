cfb51b62e90ca3fa6ec406445e3d2610d1ee6afe
MSergey86 <105745751+MSergey86@users.noreply.github.com>
2023-03-27 20:58:39 +0300

    Update task.js

--- JS_inBrowser/ElementSearch/popups/task.js
+++ JS_inBrowser/ElementSearch/popups/task.js
@@ -1,21 +1,30 @@
 
 const main = document.getElementById('modal_main');
 const close = document.getElementsByClassName('modal__close modal__close_times');
+let closeLen = close.length;
 const btnShowSucces = document.getElementsByClassName('btn btn_danger modal__close show-success');
 const success = document.getElementById('modal_success');
 
 
 main.className = 'modal modal_active';
 
-close[0].onclick = () => {
-    main.className = 'modal';
-}
-close[1].onclick = () => {
-    success.className = 'modal';
-}
-
 btnShowSucces[0].onclick = () => {
     main.className = 'modal';
     success.className = 'modal modal_active';
 }
+
+function closeClicker () {
+   for (let i = 0; i < closeLen; i++) {
+    close[i].onclick = () => {
+        if (close[i].closest('.modal_active').id = 'modal_main') {
+            main.className = 'modal';
+        }
+        if (close[i].closest('.modal_active').id = 'modal_success') {
+            success.className = 'modal';
+        }
+    }
+   }
+}
+
+closeClicker();
 
