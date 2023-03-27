d532cc5d638d3c6cd5a5c646084bd96e6a2c788f
MSergey86 <105745751+MSergey86@users.noreply.github.com>
2023-03-27 20:59:23 +0300

    Update task.js

--- JS_inBrowser/ElementSearch/slider/task.js
+++ JS_inBrowser/ElementSearch/slider/task.js
@@ -1,12 +1,12 @@
 let arr = document.querySelectorAll('.slider__item');
 let len = arr.length;
-
-let index = 0;
+let array = Array.from(arr);
 
 let prevBtn = document.getElementsByClassName('slider__arrow slider__arrow_prev');
 let nextBtn = document.getElementsByClassName('slider__arrow slider__arrow_next');
 
 prevBtn[0].onclick = () => {
+    let index = array.findIndex(item => item.className === 'slider__item slider__item_active');
     if (index > 0) {
         arr[index].className = 'slider__item';
         index--;
@@ -15,10 +15,10 @@ prevBtn[0].onclick = () => {
 }
 
 nextBtn[0].onclick = () => {
+    let index = array.findIndex(item => item.className === 'slider__item slider__item_active');
     if (index < len - 1) {
         arr[index].className = 'slider__item';
         index++;
         arr[index].className = 'slider__item slider__item_active';
     }
 }
-
