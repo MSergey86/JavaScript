4c800f94a07e46e184125b9c7b55fd295d3ac150
MSergey86 <105745751+MSergey86@users.noreply.github.com>
2023-03-27 22:04:28 +0300

    Update task.js

--- JS_inBrowser/ElementSearch/menu/task.js
+++ JS_inBrowser/ElementSearch/menu/task.js
@@ -1,57 +1,30 @@
-// 1-й вариант --------------------------
-/* const a = document.getElementsByClassName('menu__link');
-const b = document.getElementsByClassName('menu menu_sub');
-
-a[1].onclick = () => {
-    b[0].className = 'menu menu_sub menu_active';
-    b[1].className = 'menu menu_sub';
-    return false;
-}
-
-a[5].onclick = () => {
-    b[0].className = 'menu menu_sub';
-    b[1].className = 'menu menu_sub menu_active';
-    return false;
-} */
-//2-й вариант -------------------------------
 
 const arr = document.querySelectorAll('.menu__link');
-const l = arr.length;
-let j = 0;
+const len = arr.length;
+let numMenuActiv = 0;
 
 function clicker() {
-    for (let i = 0; i < l; i++) {
+    for (let i = 0; i < len; i++) {
         arr[i].onclick = () => {
-            let m = arr[i].closest('.menu__item');
-            let n = m.querySelector('.menu_sub');
-            if (n != null) {
-                if (n.className == 'menu menu_sub') {
-                    n.className = 'menu menu_sub menu_active';
-                    if (count()) {
-                        arr[j].closest('.menu__item').querySelector('.menu_sub').className = 'menu menu_sub';
-                        console.log(j);
+            let menuItem = arr[i].closest('.menu__item');
+            let menuSub = menuItem.querySelector('.menu_sub');
+            if (menuSub != null) {
+                if (menuSub.className == 'menu menu_sub') {
+                    menuSub.className = 'menu menu_sub menu_active';
+                    if (document.getElementsByClassName('menu menu_sub menu_active').length > 1) {
+                        let menuItemActive = arr[numMenuActiv].closest('.menu__item');
+                        menuItemActive.querySelector('.menu_sub').className = 'menu menu_sub';
+                        // console.log(numMenuActiv);
                     }
-                    j = i;
+                    numMenuActiv = i;
                 }
                 else {
-                    n.className = 'menu menu_sub';
+                    menuSub.className = 'menu menu_sub';
                 }
                 return false;
             }
         }
     }
-    setTimeout(clicker, 1); // ещё раз, через миллисекунду
   }
   
   clicker();
-
-  function count () {
-    let count  = document.getElementsByClassName('menu menu_sub menu_active').length;
-    if (count > 1) {
-        return true;
-    }
-    else {
-        return false;
-    }
-  }
-  
