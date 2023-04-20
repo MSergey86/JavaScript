b56fe4cc1a815f72018f4899c6b63836131c8d7c
MSergey86 <105745751+MSergey86@users.noreply.github.com>
2023-04-20 22:41:50 +0300

    Update task.js

--- JS_inBrowser/document-structure/tooltip/task.js
+++ JS_inBrowser/document-structure/tooltip/task.js
@@ -1,28 +1,36 @@
 
+
 let tagA = document.getElementsByTagName("a");
 
-// console.log(a[1].getAttribute('title'))
-
-    for (let i = 1; i < tagA.length; i ++) {
-        console.log("111111")
-        tagA[i].insertAdjacentHTML('beforeEnd',
-    `<div class="tooltip" style="left: 0; top: 0">
-    ${tagA[i].getAttribute('title')}
-    </div>`)
-    }
-
-const tooltips = document.getElementsByClassName('tooltip');
-
 function clicker() {
+    let p = 0;
     for (let i = 1; i < tagA.length; i ++) {
         tagA[i].addEventListener('click', (e) => {
-            e.preventDefault();
-            [...tooltips].forEach(tooltip => {
-                tooltip.classList = 'tooltip'
-            })
-            tooltips[i-1].classList.add('tooltip_active');
+            e.preventDefault();  
+
+            removeTooltips();
+
+            if (p != i) {
+                let top = Number(tagA[i].getBoundingClientRect().top)
+                let left = Number(tagA[i].getBoundingClientRect().left)
+
+                tagA[i].insertAdjacentHTML('afterEnd',
+                `<div class="tooltip tooltip_active" style="left: ${left}px; top: ${top+20}px">
+                ${tagA[i].getAttribute('title')}
+                </div>`)
+                p = i
+            }
+            else (p = 0)
         })
     }
 }
 
 clicker()
+
+function removeTooltips() {
+    let tooltips = document.getElementsByClassName('tooltip');
+
+    [...tooltips].forEach(tooltip => {
+      tooltip.remove()
+    })
+}
