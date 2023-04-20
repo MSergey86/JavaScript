73005d94e77c831d8c798e1d440875aab09063c5
MSergey86 <105745751+MSergey86@users.noreply.github.com>
2023-04-20 22:40:34 +0300

    Update task.js

--- JS_inBrowser/document-structure/todo/task.js
+++ JS_inBrowser/document-structure/todo/task.js
@@ -1,35 +1,33 @@
 
 const input = document.getElementById('task__input')
-const taskAdd = document.getElementById('tasks__add')
+const taskAddBtn = document.getElementById('tasks__add')
 let taskList = document.getElementById('tasks__list')
 
-taskAdd.addEventListener('click', (e) => {
+function taskAdd(e) {
     e.preventDefault()
     if (/\S/.test(input.value)) {     //проверка чтоб был хоть один символ
-        taskList.insertAdjacentHTML('beforeend',
-       `<div class="task">
-        <div class="task__title">
-        ${input.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-        </div>`
+    taskList.insertAdjacentHTML('beforeend',
+    `<div class="task">
+    <div class="task__title">
+    ${input.value}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+    </div>`
     )
     input.value = ''
     }
-})
+}
 
 function del() {
-let tasks = document.getElementsByClassName('task')
-let closeTask = document.getElementsByClassName('task__remove')
-for (let i = 0; i < closeTask.length; i++) {
-    // console.log(tasks[i].textContent)
-    closeTask[i].addEventListener('click', () => {
-        tasks[i].remove();
+    let closeTask = document.getElementsByClassName('task__remove');
+    
+    [...closeTask].forEach(element => {
+          element.addEventListener('click', (e) => {
+            e.target.closest(".task").remove();
+          })
     })
-}
 }
 
 setInterval(() => del(), 100)
 
-
-
+taskAddBtn.addEventListener("click", taskAdd);
